14d37ab099c207b20652ff2d3c4225ec3b4c4b88
valmsmith39a <valmsmith39a@gmail.com>
2016-01-07 22:38:11 -0800

    Switch statements

--- main.js
+++ main.js
@@ -22,22 +22,56 @@ function buttonClicked(event) {
 
 	var button = event.target;  // the clicked element
 
+	var input = button.innerHTML;
+
     var output = document.getElementById('output');
 
 	output.innerHTML = output.innerHTML + button.innerHTML;
 
     
-    // Switch statement
+      var hasNumber = /\d/;
+//hasNumber.test("ABC"); // false
+//hasNumber.test("Easy as 123"); // true
 
-    switch(expression) {
-       case n:
-         code block
+     //console.log('is this a number?', hasNumber.test(input) === true);
+
+// input = 6, input = *
+
+    //console.log('input is', input);
+    //console.log('is input equal to addition char?', input==='+');
+
+    switch(input) {
+       case '+':
+         console.log("its a addition!");
          break;
-       case n:
-         code block
+       case '-':
+         console.log("It's substraction");
          break;
+       case '*':
+         console.log("It's multiplicaton");
+         break;
+       case '/':
+          console.log("It's division!");
+          break;
+       case '+/-':
+          console.log("It's flip!");
+          break;
+       case '.':
+          console.log("it's decimal");
+          break;
+       case '%':
+          console.log("It's percent!");
+          break;
+       case 'AC':
+          console.log("it's clear!");
+          break;
+       case '=':
+          console.log("It's equal");
+          break;
        default:
-         default code block
+          console.log("It's a number!");
+          break;
     } 
+    
 
 }  // buttonClicked(event)
